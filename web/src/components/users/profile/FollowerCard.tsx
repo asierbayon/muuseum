5e196370baa37fc53359b491b3a51ca8b20fc34b
asierbayon <bayonasier@gmail.com>
2021-06-13 09:26:15 +0200

    feat: FollowerCard styling

--- web/src/components/users/profile/FollowerCard.tsx
+++ web/src/components/users/profile/FollowerCard.tsx
@@ -1,6 +1,6 @@
 import { Icon } from '@iconify/react';
-import pinFill from '@iconify-icons/eva/pin-fill';
 import checkmarkFill from '@iconify-icons/eva/checkmark-fill';
+import personAddOutline from '@iconify-icons/eva/person-add-outline';
 // material
 import { Box, Card, Button, Avatar, Typography } from '@material-ui/core'; // @types
 // @types
@@ -16,24 +16,25 @@ type FollowerCardProps = {
 
 export default function FollowerCard({ follower, isFollowed, onToggle }: FollowerCardProps) {
   const { fullName, username, avatar } = follower;
-  
+
   return (
-    <Card sx={{ display: 'flex', alignItems: 'center', p: 3 }}>
+    <Card
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        p: 3,
+        boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px',
+        borderRadius: '0.5rem'
+      }}
+    >
       <Avatar alt={fullName} src={avatar} sx={{ width: 48, height: 48 }} />
       <Box sx={{ flexGrow: 1, minWidth: 0, pl: 2, pr: 1 }}>
         <Typography variant="subtitle2" noWrap>
           {fullName}
         </Typography>
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <Box
-            component={Icon}
-            icon={pinFill}
-            sx={{ width: 16, height: 16, mr: 0.5, flexShrink: 0 }}
-          />
-          <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
-            {username}
-          </Typography>
-        </Box>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
+          {username}
+        </Typography>
       </Box>
       {typeof isFollowed !== undefined && (
         <Button
